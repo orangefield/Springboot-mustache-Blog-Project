39de35206ee6cc20cc4a08a527fd9bc54df859fa
field <jsw_777@naver.com>
2022-03-10 09:25:14 +0900

    [12] 글 상세보기 페이지 완료

--- src/main/java/site/metacoding/dbproject/web/PostController.java
+++ src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -1,5 +1,7 @@
 package site.metacoding.dbproject.web;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -46,8 +48,16 @@ public class PostController {
 
     // 글상세보기 페이지 /post/{id} (삭제버튼, 수정버튼 만들어 두면 됨) - 인증 X
     @GetMapping("/post/{id}") // Get요청에 /post만 제외시킬 것이다(Filter에서)
-    public String detail(@PathVariable Integer id) {
-        return "/post/detail";
+    public String detail(@PathVariable Integer id, Model model) {
+        Optional<Post> postOp = postRepository.findById(id);
+
+        if (postOp.isPresent()) {
+            Post postEntity = postOp.get();
+            model.addAttribute("post", postEntity);
+            return "post/detail";
+        } else {
+            return "error/page1";
+        }
     }
 
     // 글수정 페이지 /post/{id}/updateForm - 인증 O
