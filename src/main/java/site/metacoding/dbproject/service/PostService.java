5e0d181dd55c1ce28c56ffd10748c41aa10a8052
field <jsw_777@naver.com>
2022-03-18 12:56:18 +0900

    [22] summernote 달기

--- src/main/java/site/metacoding/dbproject/service/PostService.java
+++ src/main/java/site/metacoding/dbproject/service/PostService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
